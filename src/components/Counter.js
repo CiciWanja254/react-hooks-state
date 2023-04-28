25afb31b86ed6bf56d236f97300c4dfceba87c24
Cici254 <thukucindy00@gmail.com>
2023-04-28 21:56:53 +0300

    Complete assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,47 @@
 import React, { useState } from "react";
 
 function Counter() {
+
   const [count, setCount] = useState(0);
+  const [ value, setValue ] = useState(null);
 
   function increment() {
+    console.log(typeof count);
     setCount(count + 1);
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  function decrement() {
+    setCount(count - 1);
+  }
+
+  function handleChange(value) {
+    setValue(Number(value));
+  }
+
+  function handleSet() {
+    setCount(Number(value));
+  }
+
+  function handleReset() {
+    setCount(0);
+  }
+
+  return (<div>
+      <h1>
+        Count: {count}
+      </h1>
+      <button onClick={increment}>Add</button>
+      <br/>
+      <button onClick={decrement}>Remove</button>
+      <br/>
+      <input type={"number"} value={value} onChange={(event) => handleChange(event.target.value) } />
+      <button onClick={handleSet}>Set</button>
+      <br/>
+      <button onClick={handleReset}>Reset</button>
+
+    </div>    
+  );
+        
 }
 
 export default Counter;
